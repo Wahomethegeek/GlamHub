438dc5aa55508263a30776fc4b05274c963872b2
Wahomethegeek <gatwirikelvin@outlook.com>
2024-02-01 23:37:23 +0300

    adding product form

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.profile, name='profile'),
+    path('my_store/', views.my_store, name='my_store'),
+    path('my_store/add_product/', views.add_product, name='add_product'),
     path('vendor/<int:pk>/', views.vendor_detail, name='vendor_detail'),
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
 from userprofile.models import Userprofile
+
+from store.forms import ProductForm
+from store.models import Product, Category
 
 
 # Create your views here.
@@ -15,6 +19,28 @@ def vendor_detail(request, pk):
     })
 
 
+@login_required
+def my_store(request):
+    return render(request, 'userprofile/my_store.html')
+
+
+@login_required
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            # Save the form data to the database
+            product = form.save(commit=False)
+            product.user = request.user  # Assuming Product model has a 'user' field
+            product.save()
+            return redirect('success_page')  # Redirect to a success page or any other desired page
+    else:
+        form = ProductForm()
+
+    return render(request, 'userprofile/add_product.html', {'form': form})
+
+
+@login_required
 def profile(request):
     return render(request, 'userprofile/profile.html')
 
