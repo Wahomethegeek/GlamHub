5993200e7409d33a7b8d303643e4b343bfe174c6
Wahomethegeek <gatwirikelvin@outlook.com>
2024-02-12 16:56:46 +0300

    Increment or decrease products in the cart

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('search/', views.search, name='search'),
     path('add_to_cart/<str:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('change_cart/<str:product_id>/', views.change_quantity, name='change_quantity'),
     path('cart/', views.cart_view, name='cart_view'),
     path('remove_from_cart/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('<slug:slug>/', views.category_detail, name='category_detail'),
